f0948f90b5d38b6fe0d5b2ac976da1e8a95714ba
Joy George Kunjikkuru <joymon@gmail.com>
2021-09-30 12:07:50 -0400

    Added k8s yaml and made the exe run for ever

---
+++ commands.ps1
@@ -0,0 +1,18 @@
+﻿docker build -t joymon/message-queue-api:0.0.1 -f .\webapi\Dockerfile .
+docker push joymon/message-queue-api:0.0.1
+
+docker build -t joymon/message-queue-processor:0.0.1 -f .\message-processor\Dockerfile .
+docker push joymon/message-queue-processor:0.0.1
+
+kubectl apply -f .\webapi\k8s-docker-desktop-message-queue.yml
+
+kubectl get all -n=message-queue-k8s
+
+kubectl describe service/webapiservice -n=message-queue-k8s
+kubectl describe pod/message-processor-5dc5d94c98-ntvkb -n=message-queue-k8s
+kubectl logs pod/rabbitmq-57cbfb7848-t9wd8 -n=message-queue-k8s
+kubectl logs pod/webapi-66cb46499-fnj4h -n=message-queue-k8s
+kubectl logs pod/message-processor-5dc5d94c98-t5pbf -n=message-queue-k8s
+
+# Clean up
+kubectl delete -f .\webapi\k8s-docker-desktop-message-queue.yml
--- message-processor/Program.cs
+++ message-processor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
 using System;
 using System.Net.Http;
 using System.Text;
@@ -10,32 +11,83 @@ namespace message_processor
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine($"Started {nameof(message_processor)}");
-            ConnectionFactory factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672 };
-            factory.UserName = "guest";
-            factory.Password = "guest";
-            IConnection conn = factory.CreateConnection();
-            IModel channel = conn.CreateModel();
-            channel.QueueDeclare(queue: "myqueue",
-                                    durable: false,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
+            try
+            {
+                Console.WriteLine($"Started {nameof(message_processor)}");
+                ConnectionFactory factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672 };
+                factory.UserName = "guest";
+                factory.Password = "guest";
+                IConnection conn = factory.CreateConnection();
+                IModel channel = conn.CreateModel();
+                channel.QueueDeclare(queue: "myqueue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                //await TryUsingEventingConsumer(channel);
+                await TryUsingBasicGet(channel);
+                return;
+            }
+            catch(BrokerUnreachableException bue)
+            {
+                Console.WriteLine("Broker seems offline. Let's wait for 2 seconds");
+                await Task.Delay(TimeSpan.FromSeconds(2));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception : {ex}");
+            }
+            Console.WriteLine($"Completed {nameof(message_processor)}. Ideally this is not expected as it should run for ever.");
+        }
+
+        private static async Task TryUsingEventingConsumer(IModel channel)
+        {
+            Console.WriteLine($"{nameof(TryUsingEventingConsumer)} start");
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.Span;
                 var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine($"🠗 Received from RabbitMQ: {message}");
+                Console.WriteLine($"🠗 EventReceived from RabbitMQ: {message}");
             };
-            Console.WriteLine("Subscribed to the queue");
             channel.BasicConsume(queue: "myqueue",
                                     autoAck: true,
                                     consumer: consumer);
-            Task.Delay(1000).Wait();
-            Console.WriteLine($"Completed {nameof(message_processor)}");
+            await Task.Delay(5000);
+            Console.WriteLine("Subscribed to the queue.Waiting for messages using ReadLine...");
+            Console.ReadLine();
+            Console.WriteLine("Unfortunately the Console.ReadLine is not holding exe running...");
+        }
+        private static async Task TryUsingBasicGet(IModel channel)
+        {
+            Console.WriteLine($"{nameof(TryUsingBasicGet)} start");
+
+            while (true)
+            {
+                var queueingConsumer = new DefaultBasicConsumer(channel);
+                var result = channel.BasicGet("myqueue", false);
+
+                if (result == null)
+                {
+                    // This 100ms wait allows the processor to go do other work.
+                    // No sense in going back to an empty queue immediately. 
+                    // CancellationToken intentionally not used!
+                    // ReSharper disable once MethodSupportsCancellation
+
+                    Console.WriteLine($"{nameof(TryUsingBasicGet)} - No messages found. Sleeping for 5 secs");
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                    continue;
+                }
+                else
+                {
+                    IBasicProperties props = result.BasicProperties;
+                    ReadOnlyMemory<byte> body = result.Body;
+                    var message = Encoding.UTF8.GetString(body.Span);
+                    Console.WriteLine($"🠗 {nameof(TryUsingBasicGet)} - Dequeued from RabbitMQ: {message}");
+                }
+            }
         }
     }
 }
